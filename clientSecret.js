56d6c04c1f960dba98f3d3071a325afc166c4f6f
dern1co <nico.bolender.nb@gmail.com>
2018-09-30 10:30:32 +0200

    merge to master

---
+++ clientSecret.js
@@ -0,0 +1,16 @@
+var clientSecret = {
+    "client_id": "793729558350-58etvsoelqbc8pi5lknlven67esr03vh.apps.googleusercontent.com",
+    "project_id": "leafy-guide-132010",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://accounts.google.com/o/oauth2/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_secret": "jjgBcDv28Uqz-VaEueBX4Gwb",
+    "redirect_uri": "http://localhost:4200/auth/google/callback",
+    "client_redirect_uri": "http://localhost:4000/callback",
+    "javascript_origins": ["http://localhost:4200"],
+    "jwt_secret": "laskdjo12387aksjdhKASJDH18293KSAKLDJL",
+    "google_places_key": "AIzaSyAcLB4ukiK5M2tuVLK4P2kIyib7vUDt_r8"
+}
+
+exports.client = clientSecret;
+
