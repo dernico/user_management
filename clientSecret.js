0d49c66ad269fd9d43e89672c02db79e26ed7f6d
dern1co <nico.bolender.nb@gmail.com>
2018-09-30 12:35:46 +0200

    asd

--- clientSecret.js
+++ clientSecret.js
@@ -5,9 +5,12 @@ var clientSecret = {
     "token_uri": "https://accounts.google.com/o/oauth2/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
     "client_secret": "jjgBcDv28Uqz-VaEueBX4Gwb",
-    "redirect_uri": "http://localhost:4200/auth/google/callback",
-    "client_redirect_uri": "http://localhost:4000/callback",
-    "javascript_origins": ["http://localhost:4200"],
+    "redirect_uri": "https://hidden-taiga-84499.herokuapp.com/auth/google/callback",
+    "client_redirect_uri": "https://hidden-taiga-84499.herokuapp.com/callback",
+    "javascript_origins": ["https://hidden-taiga-84499.herokuapp.com/"],
+    // "redirect_uri": "http://localhost:4200/auth/google/callback",
+    // "client_redirect_uri": "http://localhost:4000/callback",
+    // "javascript_origins": ["http://localhost:4200"],
     "jwt_secret": "laskdjo12387aksjdhKASJDH18293KSAKLDJL",
     "google_places_key": "AIzaSyAcLB4ukiK5M2tuVLK4P2kIyib7vUDt_r8"
 }
